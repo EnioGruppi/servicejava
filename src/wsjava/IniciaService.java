18db744a89ff0bc57f01df28b8fd313c56472d1b
enio gruppi <enio.neto@aluno.faculdadeimpacta.com.br>
2022-01-23 16:09:48 -0300

    teste ws java no heroku

--- src/wsjava/IniciaService.java
+++ src/wsjava/IniciaService.java
@@ -7,7 +7,7 @@ public class IniciaService {
 	public static void main(String[] args) {
 		String port = System.getenv("PORT");
 		String host = "http://0.0.0.0:";
-		String uri = "/service?WSDL";
+		String uri = "/service?wsdl";
 		String url = host + port + uri;
 		//String url = "http://127.0.0.1:8080/wsjava/service?WSDL";
 		Service novoservice = new Service();
